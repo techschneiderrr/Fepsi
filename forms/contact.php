d91d594584b9c4bd61d366b3a04882a0b7067e8f
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2020-08-26 13:40:44 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'daninschool@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -18,7 +18,7 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  $contact->to = $'daninschool@gmail.com';
+  $contact->to = $receiving_email_address ;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
