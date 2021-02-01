dd1699d64cd2ca675aef062caa073598727d3463
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-02-01 22:14:27 +0530

    Update events.js

--- assets/js/events.js
+++ assets/js/events.js
@@ -14,7 +14,7 @@ $(function(){
 	    easing: 'ease-in-out',
 	    duration: 800,
 	  });
-xcv bfxchttps://meet.google.com/daq-rrcp-uswx
+xcv bfxchttps:
   } else {
   	
   	sr.reveal('.js--fadeInLeft', {
